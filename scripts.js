f69206b75a9a18d3f69692c9c4e3d3cd2d8d50c4
Edmar Radanovis <edradanovis@gmail.com>
2022-03-11 11:40:54 -0300

    change including async await for application to perform quote conversion in real time

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
 const button = document.getElementById('to-convert-button')
 const select = document.getElementById('currency-select')
 
-const dollars = 5.1
-const euro = 5.75
-const bitcoin = 0.0000052.toFixed(10)
+// const dollars = 5.1     // discarded... replaced by the variables below the FETCH
+// const euro = 5.75
+// const bitcoin = 0.0000052
 
-const convertValues = () => {
+const convertValues = async () => {  // include async in function ... if not arrow function
+                                     // the format would be as follows ... async function myFunction(){
+                                     //                                }   
+
     const inputReal = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    // ASYNC AWAIT =>  asynchronous code (which does not have sync)
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dollars = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+
+    //console.log(data) // => for verification in inspect
+
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
@@ -30,7 +43,10 @@ const convertValues = () => {
     }
 
     if (select.value === '₿ Bitcoin'){
-        currencyValueText.innerHTML = (`₿ ${bitcoin * inputReal}`)
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'BTC',
+        }).format(inputReal / bitcoin)
     }
 } 
 
